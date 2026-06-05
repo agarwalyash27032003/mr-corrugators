c296e7a049ed7a9edab2fffb3020926f2ac06ece
thebluepeakstudio <thebluepeakstudio@gmail.com>
2026-06-05 22:01:14 +0530

    Social Buttons + image upload

--- frontend/src/pages/ProductDetail.jsx
+++ frontend/src/pages/ProductDetail.jsx
@@ -73,7 +73,7 @@ const ProductDetail = () => {
                   key={index}
                   src={img}
                   alt={`${product.name}-${index}`}
-                  className={`absolute inset-0 w-full h-full object-contain transition-opacity duration-150 ${
+                  className={`absolute inset-0 w-full h-full object-contain transition-opacity duration-75 ${
                     index === currentIndex ? "opacity-100" : "opacity-0"
                   }`}
                 />
